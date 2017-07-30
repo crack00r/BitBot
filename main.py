04da2b08dfcacc7f82f3ab7a810e67e3587cffb0
crack00r <crack00r@gmail.com>
2017-07-30 23:38:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import time
+import datetime
+import random
+from bs4 import BeautifulSoup
+import pandas as pd
+from coinigy import *
+from classes import Cryptoping, HandlerThread, BittrexThread, PoloniexThread
+import ConfigParser
+import thread
+
+
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+
+firstRun = True
+
+lastpair = ""
+currpair = ""
+
+cryptoping = Cryptoping.Cryptoping()
+
+while True:
+    currency, exchange = cryptoping.refreshAndGetData()
+    if firstRun:
+        lastpair=currency+exchange
+        firstRun = False
+    # currpair - z.B. BTXBittrex
+    currpair=currency+exchange
+
+
+    if(lastpair!=currpair):
+        legitExchange = False
+        if (exchange == "Poloniex"):
+            legitExchange = True
+        if (exchange == "Bittrex"):
+            legitExchange = True
+        
+        if legitExchange:
+            if (exchange == "Bittrex"):
+                thread.start_new(BittrexThread.BittrexThread, (currency,))
+            if (exchange == "Poloniex"):
+                thread.start_new(PoloniexThread.PoloniexThread, (currency,))
+
+            lastpair = currpair
+    time.sleep(random.randint(1,1))
